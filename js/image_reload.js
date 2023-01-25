57b692c4915de1c0b8c632e8223d4456f039c148
Chulhong Min <chulhong.min@gmail.com>
2023-01-25 17:19:56 +0000

    update

---
+++ js/image_reload.js
@@ -0,0 +1,14 @@
+function randomImage(){
+    var images = [
+     'images/intro.jpg',
+     'images/intro_2.jpg',
+     'images/intro_3.jpg'];
+    var size = images.length;
+    var x = Math.floor(size * Math.random());
+    console.log(x);
+    var element = document.getElementsByClassName('site-hero');
+    console.log(element);
+    element[0].style["background-image"] = "url("+ images[x] + ")";
+}
+  
+document.addEventListener("DOMContentLoaded", randomImage);
