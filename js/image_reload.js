1af80ae2362db970f4b7fd643f9e6722d9e0abba
Chulhong Min <chulhong.min@gmail.com>
2023-02-07 15:56:59 +0000

    update images

--- js/image_reload.js
+++ js/image_reload.js
@@ -3,7 +3,9 @@ function randomImage(){
      'images/intro.jpg',
      'images/intro_2.jpg',
      'images/intro_3.jpg',
-     'images/intro_4.jpg'];
+     'images/intro_4.jpg',
+     'images/intro_5.jpg',
+     'images/intro_6.jpg'];
     var size = images.length;
     var x = Math.floor(size * Math.random());
     console.log(x);
